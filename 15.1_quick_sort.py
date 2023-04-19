20a497e3de507511d59f6cdb08ad9ac12eb8925b
Sachin1566 <128275663+Sachin1566@users.noreply.github.com>
2023-04-19 16:24:28 +0530

    Add files via upload

---
+++ 15.1_quick_sort.py
@@ -0,0 +1,46 @@
+## method defination of partition
+
+def partition(arr,p,q):
+    pivot=arr[p]
+    i=p
+    for j in range(i+1,q+1):
+        if arr[j]<=pivot:
+            i += 1
+            #swap between the vales of arr[i] and arr[j]
+            arr[i],arr[j]=arr[j], arr[i]
+    #final swap between the vales of arr[i] and arr[p]
+    arr[i], arr[p]=arr[p],arr[i]
+    return i
+        
+
+
+
+#method defination of quick sort
+
+def quicksort(arr,p,q):
+    if p<q:
+        #divide and conquer
+        #1.divide
+        #function callling for the partition method
+        mid=partition(arr,p,q)
+        #recursive   function call for the left subtree
+        quicksort(arr,p, mid-1)
+        #recursive   function call for the rightsubtree
+        quicksort(arr,mid+1, q)
+        
+    return arr
+
+
+
+
+
+
+
+
+#driver code
+arr=[50,70,29,67,12,15,46]
+p=0
+q=len(arr)-1
+#function callling
+result=quicksort(arr,p,q) 
+print(result)
