3a0e61c19ad0c9c5ab9c804bc857f3cdf99f64f7
Sachin1566 <128275663+Sachin1566@users.noreply.github.com>
2023-04-27 00:36:18 +0530

    Update TWO_PAIR_SUM CODE.py

--- TWO_PAIR_SUM CODE.py
+++ TWO_PAIR_SUM CODE.py
@@ -1,20 +1,51 @@
-#o(n)
-def twosum(arr,i,j,target):
-   
+#O(n)
+
+# for(i=0,i<n;i++){
+#     for(j=0;j<n;j++){
+#         if(i==j)
+#         continue;
+#         if(arr[i]+arr[j]==target)
+#     }
+# }
+
+
+
+
+
+
+# def two_sum(arr,target_sum):
+#     left=0
+#     right=len(arr)-1
+#     while left< right:
+#         if(arr[left]+ arr[right] == target_sum):
+#             return left,right
+#         elif(arr[left] + arr[right] < target_sum):
+#             left=left+1
+#         else:
+#             right=right-1
+#     return -1,-1
     
-    while i<=j:
-        if arr[i] + arr[j] == target:
-            return [i,j]
-        if arr[i] + arr[j]>target:
-            j=j-1
-        else:
-            i=i-1
+# #driver code
+
+# # arr=[20,40,60,80,90,120,240]
+# # target_sum=210   #(4,5)
+
+# # arr=[2,7,11,15]
+# # target_sum=9  #(0,1)
+
+def two_sum(arr,target_sum):
+        sums = {}
+        for i , num in enumerate(nums):
+            diff = target - num
+            if diff in sums:
+                return [sums[diff] , i]
+            else:
+                sums[num] = i
+        return -1
+
+nums=[3,2,4]
+target=6  #[1,2]
+
+result=two_sum(nums,target)
+print(result)
             
-    return [-1,-1] #if its not found
-    
-arr=[2,7,11,15]
-target=26
-i=0
-j=len(arr)-1
-result=twosum(arr,i,j,target)
-print(re
